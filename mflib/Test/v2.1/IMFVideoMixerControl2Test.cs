0d69e158fcbbf7946804eb12b11fdb3e00aa6a62
snarfle <>
2015-02-18 23:01:25 +0000

    Tests for IMFVideoMixerControl2

--- mflib/Test/v2.1/IMFVideoMixerControl2Test.cs
+++ mflib/Test/v2.1/IMFVideoMixerControl2Test.cs
@@ -4,6 +4,7 @@ using System.Text;
 using MediaFoundation;
 using MediaFoundation.Misc;
 using System.Diagnostics;
+using MediaFoundation.EVR;
 
 namespace Testv21
 {
@@ -11,6 +12,21 @@ namespace Testv21
     {
         public void DoTests()
         {
+            IMFGetService gs = (IMFGetService)new EnhancedVideoRenderer();
+            object o;
+            int hr;
+            hr = gs.GetService(MFServices.MR_VIDEO_MIXER_SERVICE, typeof(IMFVideoMixerControl2).GUID, out o);
+            MFError.ThrowExceptionForHR(hr);
+
+            IMFVideoMixerControl2 vmc = (IMFVideoMixerControl2)o;
+            hr = vmc.SetMixingPrefs(MFVideoMixPrefs.AllowDropToBob | MFVideoMixPrefs.ForceBob);
+            MFError.ThrowExceptionForHR(hr);
+
+            MFVideoMixPrefs mp;
+            hr = vmc.GetMixingPrefs(out mp);
+            MFError.ThrowExceptionForHR(hr);
+
+            Debug.Assert(mp == (MFVideoMixPrefs.AllowDropToBob | MFVideoMixPrefs.ForceBob));
         }
     }
 }
