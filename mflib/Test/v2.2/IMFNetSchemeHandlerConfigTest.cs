9e857442826c79bf6731223f391b00632e3c4481
snarfle <>
2015-03-24 01:35:04 +0000

    Tests for IMFNetSchemeHandlerConfig

--- mflib/Test/v2.2/IMFNetSchemeHandlerConfigTest.cs
+++ mflib/Test/v2.2/IMFNetSchemeHandlerConfigTest.cs
@@ -11,6 +11,29 @@ namespace Testv22
     {
         public void DoTests()
         {
+            object o;
+            int hr;
+
+            hr = MFExtern.MFCreateNetSchemePlugin(typeof(IMFSchemeHandler).GUID, out o);
+            MFError.ThrowExceptionForHR(hr);
+
+            IMFNetSchemeHandlerConfig nsc = (IMFNetSchemeHandlerConfig)o;
+
+            int i;
+            hr = nsc.GetNumberOfSupportedProtocols(out i);
+            MFError.ThrowExceptionForHR(hr);
+
+            Debug.Assert(i != 0);
+
+            for (int x = 0; x < i; x++)
+            {
+                MFNetSourceProtocolType pt;
+                hr = nsc.GetSupportedProtocolType(x, out pt);
+                MFError.ThrowExceptionForHR(hr);
+            }
+
+            hr = nsc.ResetProtocolRolloverSettings();
+            MFError.ThrowExceptionForHR(hr);
         }
     }
 }
