d7997bf3b728deea345ca319c497114aa9e8a0df
snarfle <>
2015-04-13 21:47:32 +0000

    Save partial work

--- mflib/Test/v2.2/IMFRemoteProxyTest.cs
+++ mflib/Test/v2.2/IMFRemoteProxyTest.cs
@@ -11,6 +11,36 @@ namespace Testv22
     {
         public void DoTests()
         {
+#if false
+            int hr;
+
+            IMFActivate ea;
+            IMFMediaSession ms;
+            hr = MFExtern.MFCreatePMPMediaSession(MFPMPSessionCreationFlags.None, null, out ms, out ea);
+            MFError.ThrowExceptionForHR(hr);
+
+            IMFGetService gs = (IMFGetService)ms;
+
+            object o;
+            hr = gs.GetService(MFServices.MF_PMP_SERVICE, typeof(IMFPMPHost).GUID, out o);
+            MFError.ThrowExceptionForHR(hr);
+
+            ///////////////////////////////////////////////////////
+            IMFPMPHost ph = (IMFPMPHost)o;
+            hr = ph.LockProcess();
+            MFError.ThrowExceptionForHR(hr);
+
+            hr = ph.CreateObjectByCLSID(CLSID.CLSID_MFSourceResolver, null, typeof(IMFSourceResolver).GUID, out o);
+            MFError.ThrowExceptionForHR(hr);
+
+            IMFGetService gs2 = (IMFGetService)o;
+            object o2;
+            hr = gs2.GetService(MFServices.MF_REMOTE_PROXY, typeof(IMFRemoteProxy).GUID, out o2);
+            IMFRemoteProxy rp2 = (IMFRemoteProxy)o2;
+
+            hr = ph.UnlockProcess();
+            MFError.ThrowExceptionForHR(hr);
+#endif
         }
     }
 }
