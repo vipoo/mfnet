3b5f448be9b471e0949fe338cb0bbd8b8e2ab240
snarfle <>
2015-05-16 06:17:51 +0000

    Scrubbing

--- mflib/src/mfmediaengine.cs
+++ mflib/src/mfmediaengine.cs
@@ -217,6 +217,7 @@ namespace MediaFoundation
     [Flags, UnmanagedName("MF_MEDIA_ENGINE_CREATEFLAGS")]
     public enum MF_MEDIA_ENGINE_CREATEFLAGS
     {
+        None = 0,
         AudioOnly = 0x0001,
         WaitForStableState = 0x0002,
         ForceMute = 0x0004,
