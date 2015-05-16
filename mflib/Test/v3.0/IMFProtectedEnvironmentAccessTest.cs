bb38beb759f0761f6632f07b17e3ff49ca348cb6
snarfle <>
2015-05-16 06:41:45 +0000

    IMFProtectedEnvironmentAccess

--- mflib/Test/v3.0/IMFProtectedEnvironmentAccessTest.cs
+++ mflib/Test/v3.0/IMFProtectedEnvironmentAccessTest.cs
@@ -4,6 +4,7 @@ using System.Text;
 using MediaFoundation;
 using MediaFoundation.Misc;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Testv30
 {
@@ -11,6 +12,22 @@ namespace Testv30
     {
         public void DoTests()
         {
+            int hr;
+
+            IMFProtectedEnvironmentAccess pea;
+            hr = MFExtern.MFCreateProtectedEnvironmentAccess(out pea);
+            MFError.ThrowExceptionForHR(hr);
+
+            Program.IsA(pea, typeof(IMFProtectedEnvironmentAccess).GUID);
+
+            IntPtr ip;
+            int i;
+            hr = pea.ReadGRL(out i, out ip);
+            MFError.ThrowExceptionForHR(hr);
+
+            IntPtr ipout = Marshal.AllocCoTaskMem(1000);
+            hr = pea.Call(0, IntPtr.Zero, 1000, ipout);
+            MFError.ThrowExceptionForHR(hr);
         }
     }
 }
