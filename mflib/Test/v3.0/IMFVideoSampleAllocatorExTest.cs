cf2dca874340ac95dc467e4da347c6f02edf8575
snarfle <>
2015-05-16 07:46:00 +0000

    IMFVideoSampleAllocatorEx

--- mflib/Test/v3.0/IMFVideoSampleAllocatorExTest.cs
+++ mflib/Test/v3.0/IMFVideoSampleAllocatorExTest.cs
@@ -11,6 +11,34 @@ namespace Testv30
     {
         public void DoTests()
         {
+            int hr;
+
+            object o;
+            hr = MFExtern.MFCreateVideoSampleAllocatorEx(typeof(IMFVideoSampleAllocatorEx).GUID, out o);
+            MFError.ThrowExceptionForHR(hr);
+
+            IMFVideoSampleAllocatorEx vsa = o as IMFVideoSampleAllocatorEx;
+
+            IMFMediaType imfMediaType;
+            hr = MFExtern.MFCreateMediaType(out imfMediaType);
+            MFError.ThrowExceptionForHR(hr);
+
+            hr = imfMediaType.SetGUID(MFAttributesClsid.MF_MT_MAJOR_TYPE, MFMediaType.Video);
+            MFError.ThrowExceptionForHR(hr);
+
+            hr = imfMediaType.SetGUID(MFAttributesClsid.MF_MT_SUBTYPE, MFMediaType.NV12);
+            MFError.ThrowExceptionForHR(hr);
+
+            hr = imfMediaType.SetUINT64(MFAttributesClsid.MF_MT_FRAME_SIZE, MFExtern.Pack2UINT32AsUINT64(800, 600));
+            MFError.ThrowExceptionForHR(hr);
+
+            IMFAttributes ia;
+
+            hr = MFExtern.MFCreateAttributes(out ia, 5);
+            MFError.ThrowExceptionForHR(hr);
+
+            hr = vsa.InitializeSampleAllocatorEx(1, 1, ia, imfMediaType);
+            MFError.ThrowExceptionForHR(hr);
         }
     }
 }
