797d8d6c671bff893be47ea33adbc6f919b7b5e7
snarfle <>
2015-05-22 08:53:41 +0000

    IMFMediaError test

--- mflib/Test/v3.0/IMFMediaErrorTest.cs
+++ mflib/Test/v3.0/IMFMediaErrorTest.cs
@@ -11,6 +11,27 @@ namespace Testv30
     {
         public void DoTests()
         {
+            IMFMediaEngineClassFactory mecf = new MFMediaEngineClassFactory() as IMFMediaEngineClassFactory;
+            Debug.Assert(mecf != null);
+
+            int hr;
+            IMFMediaError merr;
+
+            hr = mecf.CreateError(out merr);
+            MFError.ThrowExceptionForHR(hr);
+
+            hr = merr.SetErrorCode(MF_MEDIA_ENGINE_ERR.Aborted);
+            MFError.ThrowExceptionForHR(hr);
+
+            MF_MEDIA_ENGINE_ERR m = merr.GetErrorCode();
+            Debug.Assert(m == MF_MEDIA_ENGINE_ERR.Aborted);
+
+            hr = merr.SetExtendedErrorCode(1011);
+            MFError.ThrowExceptionForHR(hr);
+
+            int q = merr.GetExtendedErrorCode();
+
+            Debug.Assert(q == 1011);
         }
     }
 }
