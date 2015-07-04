7cf5f3c8bb118e83417c39ffdf55d16a3b501c8f
snarfle <>
2015-07-04 21:22:05 +0000

    Change copyright

--- mflib/src/MFReadWrite.cs
+++ mflib/src/MFReadWrite.cs
@@ -1,26 +1,16 @@
-﻿#region license
+﻿/* license
 
-/*
-MediaFoundationLib - Provide access to MediaFoundation interfaces via .NET
-Copyright (C) 2007
+MFReadWrite.cs - Part of MediaFoundationLib, which provide access to MediaFoundation interfaces via .NET
+
+Copyright (C) 2015, by the Administrators of the Media Foundation .NET SourceForge Project
 http://mfnet.sourceforge.net
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+This is free software; you can redistribute it and/or modify it under the terms of either:
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+a) The Lesser General Public License version 2.1 (see license.txt)
+b) The BSD License (see BSDL.txt)
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
-#endregion
 
 using System;
 using System.Text;
